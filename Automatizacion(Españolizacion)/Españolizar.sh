0897b59b3cdc4ee3decebfecbc7c9611f9404fff
Neo Armada <Neoarmadam@gmail.com>
2026-03-26 10:34:48 +0100

    Update Españolizar.sh
    Puto Windows con CRLF

--- Automatizacion(Españolizacion)/Españolizar.sh
+++ Automatizacion(Españolizacion)/Españolizar.sh
@@ -3,13 +3,12 @@
 
 # Instalar dependencias en el servidor
 instalar_dependencias(){
-    local REPO_URL="https://github.com/Neoarmadam/Proyecto_ASIR" 
-    local TARGET_DIR="/Auto_Neo" 
+    local REPO_URL="https://github.com/Neoarmadam/Proyecto_ASIR"
+    local TARGET_DIR="/Auto_Neo"
 
     echo "Se van a instalar dependencias elegidas por el gran administrador en el servidor."
     sudo dnf install git cockpit-podman cockpit-storaged cockpit-files -y
     curl -s https://install.zerotier.com | sudo bash
-    
     # Evitar error si la carpeta ya existe
     if [ ! -d "$TARGET_DIR" ]; then
         sudo git clone $REPO_URL $TARGET_DIR
