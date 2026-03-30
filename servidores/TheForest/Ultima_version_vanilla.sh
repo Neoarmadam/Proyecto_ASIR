b6a4d4a6099a5abc4681d3d40317543a6ea33cc8
Neo Armada <neoarmadam@gmail.com>
2026-03-30 21:58:17 +0200

    Servidores
    Cambio de nombres de variables

--- servidores/TheForest/Ultima_version_vanilla.sh
+++ servidores/TheForest/Ultima_version_vanilla.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Preguntas al usuario
-read -p "Nombre del contenedor: " CONT_NAME
-read -p "Ruta local para datos (ej. /opt/mc_server): " VOL_PATH
+read -p "Nombre del contenedor: " nombre
+read -p "Ruta local para datos (ej. /opt/mc_server): " carpeta
 
 # Crear directorio si no existe
-mkdir -p "$VOL_PATH"
+mkdir -p "$carpeta"
 
 # Ejecución en Podman
 echo "Desplegando servidor..."
 podman run -d \
-  --name "$CONT_NAME" \
-  -v "$VOL_PATH":/theforest:Z \
+  --name "$nombre" \
+  -v "$carpeta":/theforest:Z \
   -p 27015:27015/udp \
   -p 27016:27016/udp \
   -p 8766:8766/udp \
   --restart always \
   docker.io/jammsen/the-forest-dedicated-server:latest
 
-echo "[+] Servidor $CONT_NAME listo."
+echo "[+] Servidor $nombre listo."
--- servidores/minecraft/forge_minecraft.sh
+++ servidores/minecraft/forge_minecraft.sh
@@ -4,6 +4,7 @@
 read -p "Nombre del contenedor: " nombre
 read -p "Puerto a mapear (ej. 25565): " puerto
 read -p "Ruta local para datos (ej. /opt/mc_server): " carpeta
+read -p "Dame la version de Minecraft para el servidor: " version
 
 # Crear directorio si no existe
 mkdir -p "$carpeta"
@@ -12,9 +13,12 @@ mkdir -p "$carpeta"
 echo "Desplegando servidor..."
 podman run -d \
   --name "$nombre" \
-  -p "0.0.0.0:$puerto":25565 \
   -v "$carpeta":/data:Z \
+  -e TYPE=FORGE \
+  -e VERSION="$version" \
   -e EULA=TRUE \
+  -e MEMORY=4G \
+  -p "0.0.0.0:$puerto":25565 \
   docker.io/itzg/minecraft-server
 
 echo "[+] Servidor $nombre listo en el puerto $puerto"
