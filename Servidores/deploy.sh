b3b3a1c90b62d7346a52e08b4ca896c72fde5533
Neo Armada <Neoarmadam@gmail.com>
2026-04-08 11:04:22 +0200

    Update deploy.sh
    Correcion de rutas

--- Servidores/deploy.sh
+++ Servidores/deploy.sh
@@ -58,7 +58,7 @@ ejecutar_despliegue() {
 
 # Funcion que ejecuta el Script.
 main() {
-    local TARGET_DIR="/Auto_Neo/servidores"
+    local TARGET_DIR="/Auto_Neo/Servidores"
     
     mostrar_bienvenida
     
