7425984121b1ca6bd477cef94d45a8ef75efea9c
Neo Armada <Neoarmadam@gmail.com>
2026-04-10 13:32:02 +0200

    Update update.sh
    Añadir avisos para el usuario.

--- Automatizacion/update.sh
+++ Automatizacion/update.sh
@@ -40,6 +40,33 @@ aplicar_alias() {
     source /etc/profile.d/auto_neo.sh
 }
 
+# Funcion para dar avisos al usuario.
+avisos(){
+    local opcion
+
+    echo "--- Se está ejecutando el script: $0"
+    sleep 1
+
+    echo "- Este Script va a actualizar el repositorio de automatizacion de Neo."
+    read -p "Presiona [ENTER] para continuar o cualquier otra tecla para salir... " opcion
+
+    if [[ -n "$opcion" ]]; then
+        echo "Operación cancelada por el usuario."
+        exit 1
+    fi
+
+    echo "- Tambien se va a actualizar los alias del servidor."
+    read -p "Presiona [ENTER] para continuar o cualquier otra tecla para salir... " opcion
+
+    if [[ -n "$opcion" ]]; then
+        echo "- Operación cancelada por el usuario."
+        exit 1
+    fi
+
+    echo "- Continuando..."
+    sleep 1
+}
+
 # Funcion que ejecuta el Script. Actualiza los templates.
 main(){
     local REPO_URL="https://github.com/Neoarmadam/Proyecto_ASIR" #Ruta de mi Github.
@@ -48,6 +75,7 @@ main(){
     local ARCHIVO_ALIASTXT="$DIR_SCRIPT/alias.txt"
     local config_global="/etc/profile.d/auto_neo.sh"
 
+    avisos
     echo "--- Actualizando plantillas..."
     sudo rm -rf $TARGET_DIR
     sudo git clone $REPO_URL $TARGET_DIR
